a7586c7341506a41fc1202242227cdfa4b1af7b6
okarin <kanvetakeru4461@gmail.com>
2023-01-14 21:18:05 +0900

    css読み込み使い topページの追加 cssファイル追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
 <head>
-  <title>Laravel Sample</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <title>ErogeDLsite</title>
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" /> 
 </head>
 <body>
-  Hello World!
+  <header>
+
+  </header>
+    <div class="top-wrapper">
+      <h1>Eroge DLsite</h1>
+    </div>
+
+    <h2>Hello World!!!!!!!!!!!!!!!</h2>
+
+  <footer>
+
+  </footer>
 </body>

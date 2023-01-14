86cdbd0c6c8c5a7ea31452269e8774d1d4e0742d
pepe-shumpei <shun.515515@gmail.com>
2023-01-14 15:51:45 +0900

    create top page

---
+++ resources/views/top.blade.php
@@ -0,0 +1,7 @@
+<head>
+  <title>Laravel Sample</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+  Hello World!
+</body>
